6cdd95d256840435219f0f779b2891d2759bd879
Aziz-repo <81187136+Aziz-repo@users.noreply.github.com>
2023-10-12 20:36:54 +0100

    integration

--- prescription-front/src/components/Dashboard/AddPatient/AddPatient.js
+++ prescription-front/src/components/Dashboard/AddPatient/AddPatient.js
@@ -2,19 +2,19 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import AdminDetails from './AdminDetails';
 import { useNavigate } from 'react-router-dom';
-import { AddPatientOLD } from './AddPatient OLD';
+
 
 export default function AddPatient({ setAddingPatient }) {
   const navigate = useNavigate();
 
   const handleComplete = () => {
-    const newCompleted = AddPatientOLD.completed;
-    newCompleted[AddPatientOLD.activeStep] = true;
-    AddPatientOLD.setCompleted(newCompleted);
-    if(AddPatientOLD.allStepsCompleted()){
+    const newCompleted = completed;
+    newCompleted[activeStep] = true;
+    setCompleted(newCompleted);
+    if(allStepsCompleted()){
       setAddingPatient(false)
     }else{
-      AddPatientOLD.handleNext();
+      handleNext();
     }
   };
 
